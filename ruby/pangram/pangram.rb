5f67402c21bba2ed385f4eeed05d9e3d0d24b364
Greg Perlman <gperlman27@gmail.com>
2016-08-28 14:57:38 -0400

    Add pangram and sieve

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,21 @@
+class Pangram
+	def self.is_pangram?(str)
+		alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+		count = 0
+		alphabet.each { |letter|
+			if str.downcase.match(letter) != nil
+				count += 1
+			end
+		}
+
+		if count >= 26
+			true
+		else
+			false
+		end
+	end
+end
+
+  module BookKeeping
+    VERSION = 2 # Where the version number matches the one in the test.
+  end
---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,37 @@
+class Sieve
+	def initialize(num)
+		@num = num
+	end
+
+	def primes
+		num_list = []
+		starting_num = @num
+		prime_nums = []
+
+		if starting_num >= 5
+			(2..starting_num).each { |num|
+				num_list << num
+			}
+
+			count = 2
+			while(num_list.any?)
+				prime_nums << num_list[0]
+				num_list.select! { |num|
+					num % count != 0
+				}
+				count += 1
+			end
+		elsif starting_num == 1
+			return []
+		else
+			prime_nums << starting_num
+			return prime_nums
+		end
+
+		prime_nums.uniq!
+	end
+end
+p Sieve.new(1).primes
+p Sieve.new(4).primes
+
+p Sieve.new(20).primes
