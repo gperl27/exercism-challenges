f8d3ca4ad084201dea9ebffb7e860ece6075e402
Greg P <gperlman27@gmail.com>
2016-12-24 13:26:14 -0500

    finish robot js

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,14 +1,44 @@
+var usedNames = {};
+
 var Robot = function () {
-  this.theName = "";
+  this.name = createName();
+  this.checkNames();
 }
 
-Robot.prototype.name = function() {
-  return this.name;
+Robot.prototype.checkNames = function() {
+  while(checkRepeats(usedNames, this.name)){
+    this.name = createName();
+  }
+  usedNames[this.name] = true;
+}
+
+Robot.prototype.reset = function() {
+  this.name = createName();
+  this.checkNames();
+}
+
+function createName(){
+  var name = "";
+  name += randomChar();
+  name += randomChar();
+  name += randomNum();
+  name += randomNum();
+  name += randomNum();
+  return name;
 }
 
 function randomChar(){
   var capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return charAt(Math.floor(Math.random() * capitalLetters.length));
+  return capitalLetters[Math.floor(Math.random() * capitalLetters.length)];
+}
+
+function randomNum(){
+  var numbers = "0123456789";
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function checkRepeats(usedNames, name){
+  return usedNames[name] !== undefined;
 }
 
 module.exports = Robot;
