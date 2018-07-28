5c5c8bc430944c71073689a8018d62d6a407b8ba
gregp <gperlman27@gmail.com>
2018-07-28 14:43:19 -0700

    complete bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,42 @@
 defmodule Bob do
+  def ends_with_question_mark? str do
+    String.ends_with?(str, "?")
+  end
+
+  def is_shouting? str do
+    is_all_caps?(str) && !ends_with_question_mark?(str)
+  end
+
+  def is_all_caps? str do
+    String.equivalent?(
+      str,
+      String.upcase(str)
+    )
+  end
+
+  def ends_with_exclamation? str do
+    String.ends_with?(str, "!")
+  end
+
+  def is_empty? str do
+    String.trim(str) |> String.length === 0
+  end
+
+  def has_numbers_only? str do
+    String.match?(str, ~r/^[^a-zA-Z]*$/)
+  end
+
+  def is_not_english? str do
+    String.match?(str, ~r/[^\x00-\x7F]+/)
+  end
+
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      is_empty?(input) -> "Fine. Be that way!"
+      is_shouting?(input) && !has_numbers_only?(input) || is_not_english?(input) -> "Whoa, chill out!"
+      is_all_caps?(input) && !has_numbers_only?(input) -> "Calm down, I know what I'm doing!"
+      ends_with_question_mark?(input) -> "Sure."
+      true -> "Whatever."
     end
   end
 end
